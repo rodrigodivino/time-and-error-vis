013360e0116ee3523e8f27ca56d26670bf70bb86
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2019-07-18 10:36:25 -0300

    changed to module pattern

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-const width = 1000;
-const height = 600;
-const margin = { top: 10, left: 10, right: 10, bottom: 10 };
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
+import { cleanData } from "./utils.js";
+import { draw } from "./plot.js";
 
-const svg = d3
-  .select("svg")
-  .attr("width", width)
-  .attr("height", height);
-const plot = svg
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
-
-plot
-  .append("rect")
-  .attr("width", innerWidth)
-  .attr("height", innerHeight)
-  .attr("fill", "none")
-  .attr("stroke", "slategray");
+d3.csv("./data/testData.csv", cleanData).then(draw);
---
+++ plot.js
@@ -0,0 +1,24 @@
+function draw(data) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, left: 10, right: 10, bottom: 10 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const svg = d3
+    .select("svg")
+    .attr("width", width)
+    .attr("height", height);
+  const plot = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  plot
+    .append("rect")
+    .attr("width", innerWidth)
+    .attr("height", innerHeight)
+    .attr("fill", "none")
+    .attr("stroke", "slategray");
+}
+
+export { draw };
